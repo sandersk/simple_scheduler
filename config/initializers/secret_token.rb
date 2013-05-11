bb95839c86e88ec3c627d56298d9056bb95fdc9d
Sanders Kleinfeld <sanders@oreilly.com>
2013-05-11 17:41:13 -0400

    Initial rails files for simple scheduler.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleScheduler::Application.config.secret_token = '17111c9f09263331cba6143ce4bc2d2b69b175c8183b9cd5e3a411b836bbdb807510711d1058c7d09b83d74495554f01e3c9a4bd6637706e3219c4aeaab4513b'
