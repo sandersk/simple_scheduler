4ca151dec085c03e0a359738eff9b9ad54aef1f6
Sanders Kleinfeld <sanders@oreilly.com>
2013-05-11 18:19:07 -0400

    Updated models and views for :user_id.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,7 @@
 class Appointment < ActiveRecord::Base
-  attr_accessible :date, :endtime, :name, :person, :starttime
+  validates_presence_of :date, :endtime, :name, :user_id, :starttime
+
+  attr_accessible :date, :endtime, :name, :user_id, :starttime
 
   belongs_to :user
 end
