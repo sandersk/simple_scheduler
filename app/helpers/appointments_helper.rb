c53ad6ccfdf69901c2f0e38d371391d9481c8998
Sanders Kleinfeld <sanders@oreilly.com>
2013-05-12 17:28:41 -0400

    Better error handling for bydate view.

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -4,4 +4,12 @@ def available_users
   options_for_select(User.all.map {|user| [user.person, user.id]})  
 end
 
+def render_appointments_list(appointments)
+  if appointments.length > 0
+    render 'appointments_list'
+  else
+    '<p>No appointments</p>'.html_safe
+  end
+end
+
 end
