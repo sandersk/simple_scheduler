868b0b0e51ff73ac53b5b2db5ec13089a60b0805
Sanders Kleinfeld <sanders@oreilly.com>
2013-05-12 23:26:45 -0400

    Validation for conflicting appointments.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,27 @@
 class Appointment < ActiveRecord::Base
   validates_presence_of :date, :endtime, :name, :user_id, :starttime
-
+  validate :no_time_conflict
+  # Add validation that start time must be before end time
   attr_accessible :date, :endtime, :name, :user_id, :starttime
 
   belongs_to :user
 
   def self.by_date(date)
     self.find_all_by_date(date)
+  end
+
+  def no_time_conflict
+    # Check to make sure there are no conflicting appointments scheduled for the specified date and time block
+    # Get all existing appointments on the same date as new appointment
+    other_appointments_same_day = Appointment.by_date(date)
+    other_appointments_same_day.each do |appt|
+      # For a new appointment not to conflict, one of the following must be true:
+      # 1. Both its start time and end time must be *before the start time* of the existing appt
+      # 2. Both its start time and end time must be *after the end time* of the existing appt
+      unless ((starttime < appt.starttime) & (endtime < appt.starttime)) || ((starttime > appt.endtime) & (endtime > appt.endtime))
+        errors.add(:starttime, "Appointment conflicts with appointment #{appt.name} from #{appt.starttime} to #{appt.endtime} on #{appt.date}")
+      end
+    end
   end
 
 end
